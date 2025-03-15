a85ffd335eaa10cea0ed2030a570a465e1021757
ulyana.belyakova <u.b.2004@yandex.ru>
2025-03-15 13:26:11 +0400

    lab3 edited

--- 2 модуль/lab3/lab_3.js
+++ 2 модуль/lab3/lab_3.js
@@ -16,15 +16,65 @@ class Student {
         this.lastName = lastName;   
         this.marks = marks;         
     }
+
+    static fromJSON(jsonString) {
+        const data = JSON.parse(jsonString);
+        return new Student(
+            data.firstName, 
+            data.lastName,
+            data.marks.map(m => new Mark(m.subject, m.mark)))
+    }
+
+
+    getAverage() {
+        if (this.marks.length === 0) return 0;
+        return this.marks.reduce((sum, mark) => sum + mark.mark, 0) / this.marks.length;
+    }
+
+    getMarksBySubject(subject) {
+        return this.marks.filter(m => m.subject === subject).map(m => m.mark);
+    }
+
+
+    addMark(subject, mark) {
+        this.marks.push(new Mark(subject, mark));
+    }
+
+
+    removeMarksBySubject(subject) {
+        this.marks = this.marks.filter(m => m.subject !== subject);
+    }
+
+
+    printInfo() {
+        console.log("Студент:", this.lastName, this.firstName);
+        console.log("Оценки:");
+        this.marks.forEach(mark => {
+            console.log(`- ${mark.subject}: ${mark.mark}`);
+        });
+    }
+
+    
 }
 
-const mathMark = new Mark("Математика", 5);
-const progMark = new Mark("Программирование", 5);
+const studentJSON = `{
+    "firstName": "Василий",
+    "lastName": "Хайпов",
+    "marks": [
+        {"subject": "Математика", "mark": 5},
+        {"subject": "Программирование", "mark": 5}
+    ]
+}`;
 
-const student = new Student("Василий", "Хайпов", [mathMark, progMark]);
+const parsedStudent = Student.fromJSON(studentJSON);
 
-console.log("Студент:", student.lastName, student.firstName);
-console.log("Оценки:");
-student.marks.forEach(mark => {
-    console.log(`- ${mark.subject}: ${mark.mark}`);
-});
+parsedStudent.printInfo();
+console.log("Средняя оценка:", parsedStudent.getAverage().toFixed(1));
+
+parsedStudent.addMark("Физика", 4);
+console.log("\n+ Физика");
+parsedStudent.printInfo();
+
+parsedStudent.removeMarksBySubject("Программирование");
+console.log("\n- Программирование");
+parsedStudent.printInfo();
